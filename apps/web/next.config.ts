b49fe7f333aed3ec2beeef50e76d623addb7cc86
develzy <develzy@gmail.com>
2026-06-29 07:06:31 +0700

    fix: force webpack build to resolve next-on-pages turbopack bugs

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
-import path from "path";
 
 const nextConfig: NextConfig = {
-  outputFileTracingRoot: path.join(__dirname, "../../"),
-  turbopack: {
-    root: "../../", // Menunjuk ke root folder repository (monorepo root)
-  },
+  /* config options here */
 };
 
 export default nextConfig;
